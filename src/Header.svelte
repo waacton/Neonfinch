6be115b4873676b7271ea9e708f1c897e51a8946
Wacton <waacton@gmail.com>
2023-01-28 21:09:46 +0000

    Link title colours to image

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,6 +1,14 @@
+<!--suppress CssUnresolvedCustomProperty -->
+<script lang="ts">
+    export let colourLeft: string;
+    export let colourMiddle: string;
+    export let colourRight: string;
+</script>
+
 <div class="header">
     <div class="header-decoration"></div>
-    <div class="title">
+    <div class="title"
+         style:--colour-left="{colourLeft}" style:--colour-middle="{colourMiddle}" style:--colour-right="{colourRight}">
         <h1>neonfinch</h1>
     </div>
     <div class="header-decoration"></div>
@@ -49,23 +57,21 @@
         font-weight: 300;
 
         /* all for text-gradient */
-        background: linear-gradient(45deg, magenta, yellow, cyan);
+        background: linear-gradient(45deg, var(--colour-left) 15%, var(--colour-middle) 50%, var(--colour-right) 85%);
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
 
         /* sweet neon blur */
-        text-shadow: -4px 4px 20px magenta, 0px 0px 20px yellow, 4px -4px 20px cyan
+        text-shadow: -4px 4px 20px var(--colour-left), 0px 0px 20px var(--colour-middle), 4px -4px 20px var(--colour-right)
     }
 
-    @media (max-width: 640px)
-    {
+    @media (max-width: 640px) {
         .header-decoration {
             display: none;
         }
     }
 
-    @media (max-width: 380px)
-    {
+    @media (max-width: 380px) {
         h1 {
             letter-spacing: 1rem;
             text-indent: 1rem;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,15 +26,21 @@
         useBackgroundGradient, background1, background2,
         useBorder, border, borderOpacity
     }
+
+    $: headerProps = {
+        colourRight: foreground1,
+        colourMiddle: useForegroundGradient ? foreground2 : foreground1,
+        colourLeft: useForegroundGradient ? foreground3 : foreground1,
+    }
 </script>
 
-<Header />
+<Header {...headerProps} />
 
 <!-- webstorm doesn't like this style assignment, but handy for using variables -->
 <div class="max-width-wrapper" style:--max-width="{IMAGE_SIZE * 2}px">
     <div class="main">
         <div class="svg-wrapper">
-            <FinchSvg class="test" {...svgProps}/>
+            <FinchSvg class="test" {...svgProps} />
         </div>
 
         <div class="modifiers">
@@ -42,22 +48,22 @@
                 <legend>Foreground</legend>
                     <Checkbox label="Gradient" bind:checked={useForegroundGradient} />
                     <ColourPicker bind:value={foreground1}/>
-                    <ColourPicker bind:value={foreground2} disabled={!useForegroundGradient}/>
-                    <ColourPicker bind:value={foreground3} disabled={!useForegroundGradient}/>
+                    <ColourPicker bind:value={foreground2} disabled={!useForegroundGradient} />
+                    <ColourPicker bind:value={foreground3} disabled={!useForegroundGradient} />
             </fieldset>
 
             <fieldset>
                 <legend>Background</legend>
                     <Checkbox label="Gradient" bind:checked={useBackgroundGradient} />
-                    <ColourPicker bind:value={background1}/>
-                    <ColourPicker bind:value={background2} disabled={!useBackgroundGradient}/>
+                    <ColourPicker bind:value={background1} />
+                    <ColourPicker bind:value={background2} disabled={!useBackgroundGradient} />
             </fieldset>
 
             <fieldset>
                 <legend>Border</legend>
                     <Checkbox label="Show" bind:checked={useBorder} />
-                    <OpacityPicker label="Opacity" bind:value={borderOpacity} disabled={!useBorder}/>
-                    <ColourPicker bind:value={border} disabled={!useBorder}/>
+                    <OpacityPicker label="Opacity" bind:value={borderOpacity} disabled={!useBorder} />
+                    <ColourPicker bind:value={border} disabled={!useBorder} />
             </fieldset>
         </div>
 
